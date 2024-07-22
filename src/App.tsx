eb6c41f5f1387af80cae4464cbd6554ec3049f06
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-07-22 13:57:35 +0100

    change the App component v3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="App-logo" alt="logo" />
+        <img src={reactLogo} className="logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
     </div>
