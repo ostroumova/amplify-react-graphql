683649ef38f77403e1beef8dd87e86468228bfc9
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-07-22 13:54:10 +0100

    change the App component v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={reactLogo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
     </div>
